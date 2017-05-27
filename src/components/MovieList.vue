19e653ed40886d77b2d39305d0a105db30e423fb
Thiago Rossener <thiago.rossener@gmail.com>
2017-05-27 12:23:06 -0300

    Creating .vue files

---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="movie-list">
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+    </div>
+</template>
+<script>
+    import genres from '../util/genres';
+
+    export default {
+        props: ['genre', 'time'],
+        data() {
+            return {
+                movies: [
+                    { title: 'Pulp Fiction', genre: genres.CRIME },
+                    { title: 'Home Alone', genre: genres.COMEDY },
+                    { title: 'Rogue One', genre: genres.COMEDY }
+                ]
+            };
+
+        },
+        methods: {
+            moviePassesGenreFilter(movie) {
+                if (!this.genre.length) {
+                    return true;
+                }
+                return this.genre.find(genre => movie.genre === genre);
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies.filter(this.moviePassesGenreFilter);
+            }
+        }
+    }
+</script>
